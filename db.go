9e18d3877791683970b9acf2a7f02166bbe479f5
mrKappen <tkappen@edu.uwaterloo.ca>
2019-11-18 08:04:25 -0500

    deploy fixing

--- db.go
+++ db.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
-
+	"os"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const database string = "imageCollector"
-const URI string = "mongodb+srv://tkappen:Jesus999*@cluster0-jcnwn.mongodb.net/test"
+
+// const URI string = "mongodb+srv://tkappen:Jesus999*@cluster0-jcnwn.mongodb.net/test"
 
 func setUpDb() (*mongo.Client, error) {
 	// Set client options
-	clientOptions := options.Client().ApplyURI(URI)
+	DB_URI, ok := os.LookupEnv("DB_STRING")
+	if !ok {
+		log.Println("Connection string not found!")
+		return nil, errors.New("connection string not found")
+	}
+	clientOptions := options.Client().ApplyURI(DB_URI)
 	// Connect to MongoDB
 	db, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
